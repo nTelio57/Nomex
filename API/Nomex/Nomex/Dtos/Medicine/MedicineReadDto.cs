2b692771353d81505851420edc5102233a84a8bb
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-04-17 17:58:31 +0300

    Add medicine controller

---
+++ API/Nomex/Nomex/Dtos/Medicine/MedicineReadDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nomex.Dtos.Medicine
+{
+    public class MedicineReadDto
+    {
+        public int Id { get; set; }
+        public string Barcode { get; set; }
+        public float Price { get; set; }
+        public string Name { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public bool IsCompensated { get; set; }
+    }
+}
