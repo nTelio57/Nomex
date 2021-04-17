92fd75c44b8b5359a1a1a07b4ac3b6bc8ffe8d09
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-04-17 17:56:43 +0300

    Add user personal controller

---
+++ API/Nomex/Nomex/Dtos/UserPersonal/UserPersonalReadDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nomex.Dtos.UserPersonal
+{
+    public class UserPersonalReadDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string PersonalCode { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ API/Nomex/Nomex/Profiles/UserPersonalProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Nomex.Dtos;
+using Nomex.Dtos.UserPersonal;
+using Nomex.Models;
+
+namespace Nomex.Profiles
+{
+    public class UserPersonalProfile : Profile
+    {
+        public UserPersonalProfile()
+        {
+            CreateMap<UserPersonal, UserPersonalReadDto>();
+            CreateMap<UserPersonalCreateDto, UserPersonal>();
+            CreateMap<UserPersonalUpdateDto, UserPersonal>();
+            CreateMap<UserPersonal, UserPersonalUpdateDto>();
+        }
+    }
+}
