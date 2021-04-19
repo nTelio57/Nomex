77ede32556818eb81f126c72af9253aa3cf75d6b
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-04-19 15:47:30 +0300

    Add style constants

---
+++ Mobile/nomex/lib/utilities/constants.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+
+final kHintTextStyle = TextStyle(
+  color: Colors.white70,
+  fontFamily: 'OpenSans',
+);
+
+final kLabelStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'OpenSans',
+);
+
+final kBoxDecorationStyle = BoxDecoration(
+  color: Colors.orange[200],
+  borderRadius: BorderRadius.circular(10.0),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.black12,
+      blurRadius: 6.0,
+      offset: Offset(0, 2),
+    ),
+  ],
+);
