f511080374600b70c7d35bbba38e2eaec43101ed
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-04-19 15:45:19 +0300

    Add flutter project

---
+++ Mobile/nomex/lib/main.dart
@@ -0,0 +1,20 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+import './WelcomeScreen.dart';
+import './LoginScreen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      home: WelcomeScreen(),
+    );
+  }
+}
