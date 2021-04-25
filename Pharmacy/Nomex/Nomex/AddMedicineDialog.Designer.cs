7b098df819c6f81db5915bda917b79276840955c
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-04-25 03:10:43 +0300

    Add new medicine modal

---
+++ Pharmacy/Nomex/Nomex/AddMedicineDialog.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Nomex
+{
+    partial class AddMedicineDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MedicineBarcodeText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(38, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Prekės kodas:";
+            // 
+            // MedicineBarcodeText
+            // 
+            this.MedicineBarcodeText.Location = new System.Drawing.Point(38, 47);
+            this.MedicineBarcodeText.Name = "MedicineBarcodeText";
+            this.MedicineBarcodeText.Size = new System.Drawing.Size(302, 23);
+            this.MedicineBarcodeText.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Patvirtinti";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(214, 130);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 38);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Atšaukti";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // AddMedicineDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 191);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MedicineBarcodeText);
+            this.Controls.Add(this.label1);
+            this.Name = "AddMedicineDialog";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Įvesti prekės kodą";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox MedicineBarcodeText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ Pharmacy/Nomex/Nomex/AddMedicineDialog.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nomex
+{
+    public partial class AddMedicineDialog : Form
+    {
+        private Form1 _parentForm;
+        public AddMedicineDialog()
+        {
+            InitializeComponent();
+        }
+
+        public AddMedicineDialog(Form1 parent)
+        {
+            InitializeComponent();
+            _parentForm = parent;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Medicine medicine = _parentForm.ApiControl.GetMedicineByBarcode(MedicineBarcodeText.Text);
+            if (medicine != null)
+            {
+                _parentForm.AddMedicineManually(medicine);
+                Close();
+            }
+            else
+            {
+                errorProvider1.SetError(label1, "Prekės kodas nerastas");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
