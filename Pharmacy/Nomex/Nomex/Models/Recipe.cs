3a1d93bf2dffd9924b545786e6150b069afa6a29
nTelio57 <48459027+nTelio57@users.noreply.github.com>
2021-05-05 05:03:36 +0300

    Remove user personal classes

---
+++ Pharmacy/Nomex/Nomex/Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nomex.Models
+{
+    public class Recipe
+    {
+        public int UserId { get; set; }
+        public int MedicineId { get; set; }
+        public int UsageId { get; set; }
+        public Medicine Medicine { get; set; }
+        public Usage Usage { get; set; }
+    }
+}
